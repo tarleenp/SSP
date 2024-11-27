48808a9acc2a3e38701d9b76df8ea204b9161eca
tarleenp <tarleen2003p@gmail.com>
2024-11-27 10:05:35 +0530

    Create student.java

---
+++ student.java
@@ -0,0 +1,20 @@
+package p1;
+
+public class Student {
+	private String name;
+	private int rollno;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	
+
+}
