22130f5af1a852c669e8dbb6d90053e323ec94a5
tarleenp <tarleen2003p@gmail.com>
2024-11-27 09:19:54 +0530

    Delete Matrix.java

--- Matrix.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.*;
-public interface Matrix extends Remote
-{
- public int[][] matAdd(int a[][], int b[][]) throws RemoteException;
-public int[][] matSub(int a[][], int b[][]) throws RemoteException;
-}
